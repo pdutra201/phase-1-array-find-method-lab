cc2ce0d52a27c3e9d7fa5e9d6e5813e46a6e0f6b
philip dutra <monsterphilip10@gmail.com>
2023-03-28 14:12:08 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array){const win = array.find(({result}) => result === "W");
+    if(win){
+        return win.year;
+    }
+    else{
+        return undefined;
+    }
+}
